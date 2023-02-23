c860d121556868dcbc88f9eeaf76c0d55a2584d3
shuu11 <shuu1104main@gmail.com>
2023-02-23 18:36:51 +0900

    :poop:

--- src/features/lib/dnd-kit/arrayControl.ts
+++ src/features/lib/dnd-kit/arrayControl.ts
@@ -1,17 +1,21 @@
 import { arrayMove } from '@dnd-kit/sortable'
 import type { StateDnd, StateDndCard } from '@s/recoil/stateDnd'
 
-export function removeCardIndex(item: StateDnd, id: string): StateDnd {
+export function removeCardIndex<T extends StateDnd>(item: T, id: string): T {
 	return { ...item, dndCard: item.dndCard.filter((card) => card.uuid !== id) }
 }
 
-export function insertCardIndex(item: StateDnd, oldCard: StateDndCard, newIndex: number): StateDnd {
+export function insertCardIndex<T extends StateDnd>(
+	item: T,
+	oldCard: StateDndCard,
+	newIndex: number,
+): T {
 	return {
 		...item,
 		dndCard: [...item.dndCard.slice(0, newIndex), oldCard, ...item.dndCard.slice(newIndex)],
 	}
 }
 
-export function moveCard(item: StateDnd, oldIndex: number, newIndex: number): StateDnd {
+export function moveCard<T extends StateDnd>(item: T, oldIndex: number, newIndex: number): T {
 	return { ...item, dndCard: arrayMove(item.dndCard, oldIndex, newIndex) }
 }
